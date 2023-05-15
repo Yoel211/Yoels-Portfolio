3a78009db09e371b80c7afbcea22c82e197a8726
Yoel Regalado <yoelregalado7@gmail.com>
2023-05-14 20:38:21 -0600

    added code to nav and portfolio container

---
+++ src/componets/Nav.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+          //*  This is a ternary operator that is checking if the currentPage is equal to 'Home' and if it is, it will set the className to 'nav-link active' and if it is not, it will set the className to 'nav-link'
+
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          //  TODO: Add a comment explaining what this logic is doing
+          
+
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#profolio"
+          onClick={() => handlePageChange('Profolio')}
+          //  TODO: Add a comment explaining what this logic is doing
+
+          className={currentPage === 'Porfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
--- src/componets/PortfolioContainer.js
+++ src/componets/PortfolioContainer.js
@@ -1,37 +1,33 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // TODO: Add a comment describing the functionality of this method
   // This method is responsible for rendering the appropriate component
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
-    return <Contact />;
+    if (currentPage === 'Profolio') {
+      return <Portfolio />;
+    }
+    return <Resume />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-      {/* // TODO: Add a comment describing what we are passing as props */}
-      {/* We are passing the currentPage state and the handlePageChange function as props */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* // TODO: Add a comment explaining what is happening on the following line */}
-      {/* We are calling the renderPage function to render the appropriate component */}
       {renderPage()}
     </div>
   );
