31b7b31e7d29115f831d6a6ab7353bf17f403881
Yoel Regalado <yoelregalado7@gmail.com>
2023-05-17 17:01:29 -0600

    fixed production issue

---
+++ src/components/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const stylesheet = {
+            header: {
+                backgroundColor: "purple",
+                color: "yellow", border: "2px solid green",
+                marginright: "20px",
+                backgroudposition: "right"
+            }
+        }
+
+export default function About() {
+  return (
+    <div>
+      <h1 style={stylesheet.header}>About Me</h1>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/index.js
@@ -0,0 +1,19 @@
+// export default function Header(props) {
+
+//     const stylesheet = {
+//         header: {
+//             backgroundColor: "purple",
+//             color: "yellow", border: "2px solid green",
+//             marginright: "20px",
+//             backgroudposition: "right"
+//         }
+//     }
+
+
+//     return (
+//         <h1 style={stylesheet.header}>
+//             Yoel Regalado
+//         </h1>
+//     )
+
+// }
