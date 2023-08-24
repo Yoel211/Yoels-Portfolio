abb89a99fbdbfa153bbd1367f11330fc14ac844d
Yoel Regalado <yoelregalado7@gmail.com>
2023-08-23 23:02:01 -0600

    updating icons

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,34 +4,42 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
+
+      <p> This is my Portfolio Page</p> 
+
       <a className= 'pic-1'
       href="https://github.com/Yoel211/Stock-Watchers-" target="_blank">
       <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
+      <p> This is my Portfolio Page</p>
       <a className= 'pic-1'
       href="https://github.com/Diamante710/how_to_make_your_kids_hate_you" target="_blank">
-      <img src={require("../../Assets/images/flying-dragon.png")} alt="Cinque Terre"></img>
+      <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
+      <p> This is my Portfolio Page</p>
       <a className= 'pic-1'
       href="https://github.com/Yoel211/Challenge-1" target="_blank">
-      <img src={require("../../Assets/images/dragon-head.png")} alt="Cinque Terre"></img>
+      <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
+      <p> This is my Portfolio Page</p>
       <a className= 'pic-1'
        href="https://github.com/Yoel211/redesigned-octo-funicular" target="_blank">
-      <img src={require("../../Assets/images/hydra.png")} alt="Cinque Terre"></img>
+      <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
+      <p> This is my Portfolio Page</p>
       <a className= 'pic-1'
       href="https://github.com/Yoel211/BookFace" target="_blank">
-      <img src={require("../../Assets/images/hydra.png")} alt="Cinque Terre"></img>
+      <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
+      <p> This is my Portfolio Page</p>
       <a className= 'pic-1'
       href="https://github.com/Yoel211/yoels-employee-tracker" target="_blank">
-      <img src={require("../../Assets/images/hydra.png")} alt="Cinque Terre"></img>
+      <img src={require("../../Assets/images/case-management.png")} alt="Cinque Terre"></img>
       </a>
 
     </div>
